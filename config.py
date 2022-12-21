f4038f63d0a0d2f97c17a7551190605e5ab78242
ArtemL87 <117520370+ArtemL87@users.noreply.github.com>
2022-12-21 15:47:13 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-TOKEN = '5845119076:AAHngtFBjh22snRCGa9KR1igauh4qX4i3vM'
+TOKEN = '5845119076:AAHngtFBjh22snRCGa9KR1igauh4qX4i3vM' # токен вашего телеграмм 
 
+# список валют
 keys = {
     'биткоин':'BTC',
     'эфириум':'ETH',
